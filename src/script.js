e08b43ee4e4fd81ce1563f446713e4e1642b571e
viniciuss1203@outlook.com <viniciuss1203@outlook.com>
2024-07-16 22:27:56 -0300

    adicionando alterações

--- src/script.js
+++ src/script.js
@@ -3,6 +3,41 @@
 
 //ESCREVA SEU CÓDIGO AQUI!!
 
+function Calculadora(numero1, numero2, operador){
+  let resultado
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2;
+      break;
+    case "-":
+      resultado = num1 - num2;
+      break;
+    case "/":
+      resultado = num1 / num2;
+      break;
+    case "*":
+      resultado = num1 * num2;
+      break;
+    case "e":
+      let contador = 1
+      resultado = num1 * num2
+      while (contador < num2 - 1) {
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  }
+
+  if (resultado == undefined || resultado > 1000000) {
+    resultado = "ERRO"
+  }
+  return resultado
+  
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
